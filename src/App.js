5492924d3921d44f9847842791c3d47960bfc587
DataPointBeing <jcunown@live.com>
2024-01-29 16:42:59 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Gaming!</p>
-      </header>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<div>HOME</div>} />
+                <Route path="/learn" element={<div>LEARN</div>} />
+                <Route path="/practice" element={<div>PRACTICE</div>} />
+                <Route path="/schedule" element={<div>SCHEDULE</div>} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
+
+
 
 export default App;
