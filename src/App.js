6f9864c66f1ec0a99e0e28feedd01d1fd46ca1dc
Ben Gelinas <benjamenjalinas@gmail.com>
2024-02-01 00:18:41 -0500

    Some deploy fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Practice from "./pages/practice/Practice";
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route path="/imgd4600-pet-grooming-app/" element={<div>HOME</div>} />
                 <Route path="/imgd4600-pet-grooming-app/learn" element={<div>LEARN</div>} />
                 <Route path="/imgd4600-pet-grooming-app/practice" element={<Practice />} />
                 <Route path="/imgd4600-pet-grooming-app/schedule" element={<div>SCHEDULE</div>} />
             </Routes>
-        </BrowserRouter>
+        </Router>
     </div>
   );
 }
